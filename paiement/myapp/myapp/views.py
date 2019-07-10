1bc36911b3218112afb18d6115d21e7372fcef82
Tom Galbert <antimeam@gmail.com>
2019-07-10 21:02:06 +0200

    Add info url

--- paiement/myapp/myapp/views.py
+++ paiement/myapp/myapp/views.py
@@ -5,3 +5,6 @@ from apipkg import api_manager as api
 def index(request):
     time = api.send_request('scheduler', 'clock/time')
     return HttpResponse("L'heure de la clock est %r" % time)
+
+def info(request):
+	return HttpResponse("Gestion Paiement")
